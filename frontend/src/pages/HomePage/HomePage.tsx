50d3aa695aa56ce2be5552552ef080f5391274bc
Nam Anh <nguyentrongnamanh@gmail.com>
2024-07-15 22:42:36 +0700

    15/7/2024

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -39,15 +39,149 @@ function HomePage() {
               <div className={style.SubColumnTextWrap}>
                 <h5>f/e/b/e/i/p</h5>
                 <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h6>
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                  malesuada.
+                </h6>
+              </div>
+
+              <div className={style.ContentSeparator}></div>
+
+              <div className={style.SubColumnTextWrap}>
+                <h5>f/e/b/e/i/p</h5>
+                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h6>
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                  malesuada.
+                </h6>
+              </div>
+
+              <div className={style.ContentSeparator}></div>
+
+              <div className={style.SubColumnTextWrap}>
+                <h5>f/e/b/e/i/p</h5>
+                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h6>
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                  malesuada.
+                </h6>
               </div>
             </div>
 
             <span className={style.ColumnDivider}></span>
 
-            <div className={style.SubColumn}>col3</div>
+            <div className={style.SubColumn}>
+              <div className={style.SubColumnTextWrap} style={{margin: '0 0 3% 0'}}>
+                <h5>f/e/b/e/i/p</h5>
+                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h6>
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                  malesuada.
+                </h6>
+              </div>
+
+              <div className={style.ContentSeparator}></div>
+
+              <div className={style.SubColumnTextWrap}>
+                <h5>f/e/b/e/i/p</h5>
+                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h6>
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                  malesuada.
+                </h6>
+              </div>
+
+              <div className={style.ContentSeparator}></div>
+
+              <img
+                src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
+                alt=""
+              />
+
+              <div className={style.SubColumnTextWrap}>
+                <h5>f/e/b/e/i/p</h5>
+                <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+                <h6>
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                  malesuada.
+                </h6>
+              </div>
+            </div>
           </div>
         </div>
       </Body>
+
+      <div className={style.SectionDivider}></div>
+
+      <div className={style.TrendingWrap}>
+        <div className={style.TrendingTitleWrap}>
+          <div className={style.TrendingTitleSides}></div>
+          <h1>Trending</h1>
+          <div className={style.TrendingTitleSides}></div>
+        </div>
+
+        <div className={style.TrendingContentWrap}>
+          <div className={style.TrendingPost}>
+            <img
+              src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
+              alt=""
+            />
+
+            <div className={style.TrendingTextWrap}>
+              <h5>f/e/b/e/i/p</h5>
+              <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+              <h6>
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                malesuada.
+              </h6>
+            </div>
+          </div>
+
+          <div className={style.TrendingPost}>
+            <img
+              src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
+              alt=""
+            />
+
+            <div className={style.TrendingTextWrap}>
+              <h5>f/e/b/e/i/p</h5>
+              <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+              <h6>
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                malesuada.
+              </h6>
+            </div>
+          </div>
+
+          <div className={style.TrendingPost}>
+            <img
+              src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
+              alt=""
+            />
+
+            <div className={style.TrendingTextWrap}>
+              <h5>f/e/b/e/i/p</h5>
+              <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+              <h6>
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
+                malesuada.
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.SectionDivider}></div>
+      
       <Footer />
     </div>
   );
