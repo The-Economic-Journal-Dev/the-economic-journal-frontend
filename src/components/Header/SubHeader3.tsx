bbcfba1e061f3bce457ddeea7365e5d49508ab51
Luck <luckyman3103@gmail.com>
2024-07-26 21:32:45 +0700

    Responsive nav bar

--- src/components/Header/SubHeader3.tsx
+++ src/components/Header/SubHeader3.tsx
@@ -1,19 +1,28 @@
-import "./SubHeader3.css"
+import React, { useState } from "react";
+import "./SubHeader3.css";
 
 function SubHeader3() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-      <nav className="HeaderNav">
-      <ul>
-          <li><a href="/Finance">Finance</a></li>
-          <li><a href="/Economic">Economic</a></li>
-          <li><a href="/Business">Business</a></li>
-          <li><a href="/Entrepreneur">Entrepreneurship</a></li>
-          <span className="separator"></span>
-          <li><a href="#">Contact</a></li>
-          <li><a href="/ModPage">ModPage</a></li>
-      </ul>
-  </nav>
-    )
+        <nav className="HeaderNav">
+            <button className="menuToggle" onClick={toggleMenu}>
+                ☰
+            </button>
+            <ul className={`menu ${menuOpen ? "open" : ""}`}>
+                <li><a href="/Finance">Finance</a></li>
+                <li><a href="/Economic">Economic</a></li>
+                <li><a href="/Business">Business</a></li>
+                <li><a href="/Entrepreneur">Entrepreneurship</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="/ModPage">ModPage</a></li>
+            </ul>
+        </nav>
+    );
 }
 
 export default SubHeader3;
