4dd1ac4aaa3728a68a40c7b5836b867dbb2cb74f
d8rp <thanh062007gmail.com>
2024-08-18 02:52:54 +0700

    reverted homepage to 16-06-2024 and made a skeleton loading screen

--- src/pages/HomePage/components/MainColumn.tsx
+++ src/pages/HomePage/components/MainColumn.tsx
@@ -31,10 +31,19 @@ function MainColumn({ article }: { article: IArticleData | null }) {
   useEffect(() => {
     if (article) {
       const updateLastDate = () => {
-        if (new Date(article.lastUpdated).getTime() > (new Date(article.datePublished).getTime() + 20000)) {
-          setLastDate("Last Updated: " + new Date(article.lastUpdated).toLocaleDateString());
+        if (
+          new Date(article.lastUpdated).getTime() >
+          new Date(article.datePublished).getTime() + 20000
+        ) {
+          setLastDate(
+            "Last Updated: " +
+              new Date(article.lastUpdated).toLocaleDateString()
+          );
         } else {
-          setLastDate("Date Published: " + new Date(article.datePublished).toLocaleDateString());
+          setLastDate(
+            "Date Published: " +
+              new Date(article.datePublished).toLocaleDateString()
+          );
         }
       };
 
@@ -43,34 +52,45 @@ function MainColumn({ article }: { article: IArticleData | null }) {
     }
   }, [article]);
 
-  if (isLoading) {
-    return (
-      <div className={style.MainColumn}>
-        <div className={style.Loader}></div>
-      </div>
-    );
-  }
-
-  if (!article) {
-    return <div className={style.MainColumn}>No article available</div>;
-  }
-
   return (
-    
-    <Link to={article.metaTitle ? `/articles/${article.metaTitle}` : "./"} className={style.MainColumn}>
-      <img
-        src={article.imageUrl}
-        alt=""
-        loading="lazy"
-        referrerPolicy="no-referrer"
-      />
-      <div className={style.MainColumnTextWrap}>
-        <h5>{lastDate}</h5>
-        <h1>{article.title}</h1>
-        <h6>
-          {article.summary ? article.summary : cropText(article.articleText || '')}
-        </h6>
-      </div>
+    <Link
+      to={article ? `/articles/${article.metaTitle}` : "./"}
+      className={style.MainColumn}
+    >
+      {isLoading ? (
+        <div className={style.Skeleton}>
+          <div className={style.SkeletonImage} />
+          <div className={style.SkeletonTextWrap}>
+            <div className={style.SkeletonDate} />
+            <div className={style.SkeletonTitle} />
+            <div className={style.SkeletonSummary} />
+          </div>
+        </div>
+      ) : (
+        <>
+          <img
+            src={
+              article?.imageUrl ||
+              "https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
+            }
+            alt=""
+            loading="lazy"
+            referrerPolicy="no-referrer"
+          />
+          <div className={style.MainColumnTextWrap}>
+            <h5>{lastDate || "YYYY/MM/DD"}</h5>
+            <h1>{article?.title || "No Title"}</h1>
+            <h6>
+              {article
+                ? article.summary
+                : cropText(
+                    article!.articleText ||
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                  )}
+            </h6>
+          </div>
+        </>
+      )}
     </Link>
   );
 }
