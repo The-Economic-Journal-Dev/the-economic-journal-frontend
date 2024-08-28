b545210e72a8870bd420b321d72c6437b0ef4928
d8rp <thanh062007gmail.com>
2024-08-28 22:37:40 +0700

    make trending tab works

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,24 +4,89 @@ import {useEffect, useState} from "react";
 import {TrendingTitleDecoration} from "./components/TrendingTitleDecoration.tsx";
 import MainColumn from "./components/MainColumn.tsx";
 import {SubColumnBox} from "./components/SubColumns.tsx";
+import {Link} from "react-router-dom";
+
+function TrendingPost({article}: { article: IArticleData | null }) {
+    const [lastDate, setLastDate] = useState<string>("");
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (article) {
+            const updateLastDate = () => {
+                if (
+                    new Date(article.lastUpdated).getTime() >
+                    new Date(article.datePublished).getTime() + 40000
+                ) {
+                    setLastDate(
+                        "Last Updated: " +
+                        new Date(article.lastUpdated).toLocaleDateString()
+                    );
+                } else {
+                    setLastDate(
+                        "Date Published: " +
+                        new Date(article.datePublished).toLocaleDateString()
+                    );
+                }
+            };
+
+            updateLastDate();
+            setIsLoading(false);
+        }
+    }, [article]);
+
+    return (
+        <Link to={article?.metaTitle ? `/articles/${article.metaTitle}` : "./"} className={style.TrendingPost}>
+            {isLoading ? (
+                <div className={style.Skeleton}>
+                    <div className={style.SkeletonImage}/>
+                    <div className={style.SkeletonTextWrap}>
+                        <div className={style.SkeletonDate}/>
+                        <div className={style.SkeletonTitle}/>
+                        <div className={style.SkeletonSummary}/>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <img src={article?.imageUrl} alt=""
+                         loading="lazy" referrerPolicy="no-referrer"/>
+
+                    <div className={style.TrendingTextWrap}>
+                        <h5>{lastDate}</h5>
+                        <h4>{article?.title}</h4>
+                        <h6>
+                            {article ? article.summary : (article!.articleText.slice(0, 320) + "..." || "No Body")}
+                        </h6>
+                    </div>
+                </>
+            )}
+        </Link>
+    )
+}
 
 function HomePage() {
-    const [apiData, setAPIData] = useState<IArticleData[]>([]);
-    const url = "https://api.theeconomicjournal.org/articles?includeText=true";
+    const [fetchedArticles, setFetchedArticles] = useState<IArticleData[]>([]);
+    const [trendingArticles, setTrendingArticles] = useState<IArticleData[]>([]);
+    const url = "https://api.theeconomicjournal.org/articles?includeText=true&includeTrending=true";
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const response = await fetch(url);
-                const posts = (await response.json()).articles as IArticleData[];
+                const data = await response.json()
 
-                setAPIData(posts);
+                if (!data.success) throw new Error("Error fetching posts");
+
+                const articles = data.articles as IArticleData[];
+                const trending = data.trending as IArticleData[];
+
+                setFetchedArticles(articles);
+                setTrendingArticles(trending)
             } catch (error) {
                 console.error("Error fetching posts:", error);
             }
         };
 
-        fetchPosts();
+        fetchPosts().then(() => console.log("Articles fetched"));
     }, []);
 
     return (
@@ -29,34 +94,34 @@ function HomePage() {
             <Body>
                 <div className={style.PostAreaWrap}>
                     <div className={style.PostArea}>
-                        <MainColumn article={apiData[0] || ""}/>
+                        <MainColumn article={fetchedArticles[0] || ""}/>
 
                         <span className={style.ColumnDivider}></span>
 
                         <div className={style.SubColumn}>
-                            <SubColumnBox article={apiData[1] || ""}/>
+                            <SubColumnBox article={fetchedArticles[1] || ""}/>
 
                             {/* <div className={style.ContentSeparator}></div> */}
 
-                            <SubColumnBox article={apiData[2] || ""}/>
+                            <SubColumnBox article={fetchedArticles[2] || ""}/>
 
                             {/* <div className={style.ContentSeparator}></div> */}
 
-                            <SubColumnBox article={apiData[3] || ""}/>
+                            <SubColumnBox article={fetchedArticles[3] || ""}/>
                         </div>
 
                         <span className={style.ColumnDivider}></span>
 
                         <div className={style.SubColumn}>
-                            <SubColumnBox article={apiData[4] || ""}/>
+                            <SubColumnBox article={fetchedArticles[4] || ""}/>
 
                             {/* <div className={style.ContentSeparator}></div> */}
 
-                            <SubColumnBox article={apiData[5] || ""}/>
+                            <SubColumnBox article={fetchedArticles[5] || ""}/>
 
                             {/* <div className={style.ContentSeparator}></div> */}
 
-                            <SubColumnBox article={apiData[6] || ""}/>
+                            <SubColumnBox article={fetchedArticles[6] || ""}/>
                         </div>
                     </div>
                 </div>
@@ -72,60 +137,11 @@ function HomePage() {
                 </div>
 
                 <div className={style.TrendingContentWrap}>
-                    <div className={style.TrendingPost}>
-                        <img
-                            src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
-                            alt=""
-                        />
-
-                        <div className={style.TrendingTextWrap}>
-                            <h5>f/e/b/e/i/p</h5>
-                            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                            <h6>
-                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
-                                malesuada.
-                            </h6>
-                        </div>
-                    </div>
-
-                    <div className={style.TrendingPost}>
-                        <img
-                            src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
-                            alt=""
-                        />
-
-                        <div className={style.TrendingTextWrap}>
-                            <h5>f/e/b/e/i/p</h5>
-                            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                            <h6>
-                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
-                                malesuada.
-                            </h6>
-                        </div>
-                    </div>
-
-                    <div className={style.TrendingPost}>
-                        <img
-                            src="https://biggardenfurniture.com.au/wp-content/uploads/2018/08/img-placeholder.png"
-                            alt=""
-                        />
-
-                        <div className={style.TrendingTextWrap}>
-                            <h5>f/e/b/e/i/p</h5>
-                            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
-                            <h6>
-                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                aliqua. Tincidunt nunc pulvinar sapien et ligula ullamcorper
-                                malesuada.
-                            </h6>
-                        </div>
-                    </div>
+                    <TrendingPost article={trendingArticles[0] || ""}/>
+                    <TrendingPost article={trendingArticles[1] || ""}/>
+                    <TrendingPost article={trendingArticles[2] || ""}/>
                 </div>
             </div>
-
-            {/* <div className={style.SectionDivider}></div> */}
         </div>
     );
 }
