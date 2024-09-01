0a234b4a2c0db8827994d93f8f217188dc3ab08a
d8rp <thanh062007gmail.com>
2024-09-02 03:32:35 +0700

    feat: allow image to be added in the article with fault tolerant

--- src/pages/ModPage/ModPage.tsx
+++ src/pages/ModPage/ModPage.tsx
@@ -1,65 +1,65 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "react-quill/dist/quill.snow.css";
 import Post from "./Components/Post";
 import OptionSelector from "./Components/OptionSelector";
 import style from "./ModPage.module.css";
 import Delete from "./Components/Delete";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import { auth } from "../../firebase";
+import {Helmet, HelmetProvider} from "react-helmet-async";
+import {auth} from "../../firebase";
 import NotFound from "../Errors/NotFound";
 
 const ModPage = () => {
-  const options = ["Post", "Delete"];
-  const [optionChose, setOptionChose] = useState("Post");
-  const [hasPermission, setHasPermission] = useState<Boolean>(false);
+    const options = ["Post", "Delete"];
+    const [optionChose, setOptionChose] = useState("Post");
+    const [hasPermission, setHasPermission] = useState<boolean>(false);
 
-  const optionPressed = (option: string) => {
-    setOptionChose(option);
-  };
-
-  useEffect(() => {
-    const CheckPermission = async () => {
-      const idToken = await auth.currentUser?.getIdTokenResult();
-
-      if (!idToken) return setHasPermission(false);
-
-      if (["writer", "admin"].includes((idToken.claims.role) as string)) {
-        setHasPermission(true);
-      } else {
-        setHasPermission(false);
-      }
+    const optionPressed = (option: string) => {
+        setOptionChose(option);
     };
 
-    CheckPermission();
-  }, []);
+    useEffect(() => {
+        const CheckPermission = async () => {
+            const idToken = await auth.currentUser?.getIdTokenResult();
 
-  if (!hasPermission) {
-    return <NotFound />;
-  }
+            if (!idToken) return setHasPermission(false);
 
-  return (
-    <main className={style.modForm}>
-      <HelmetProvider>
-        <Helmet>
-          <title>Modpage</title>
-          <meta name="robots" content="follow, noarchive, noindex" />
-        </Helmet>
-      </HelmetProvider>
+            if (["writer", "admin"].includes((idToken.claims.role) as string)) {
+                setHasPermission(true);
+            } else {
+                setHasPermission(false);
+            }
+        };
 
-      <div className={style.optionSelectorWrap}>
-        <OptionSelector
-          options={options}
-          optionChose={optionChose}
-          optionPressed={optionPressed}
-        />
-      </div>
+        CheckPermission();
+    }, []);
 
-      <div className={style.contentWrap}>
-        {optionChose === "Post" && <Post />}
-        {optionChose === "Delete" && <Delete />}
-      </div>
-    </main>
-  );
+    if (!hasPermission) {
+        return <NotFound/>;
+    }
+
+    return (
+        <main className={style.modForm}>
+            <HelmetProvider>
+                <Helmet>
+                    <title>Modpage</title>
+                    <meta name="robots" content="follow, noarchive, noindex"/>
+                </Helmet>
+            </HelmetProvider>
+
+            <div className={style.optionSelectorWrap}>
+                <OptionSelector
+                    options={options}
+                    optionChose={optionChose}
+                    optionPressed={optionPressed}
+                />
+            </div>
+
+            <div className={style.contentWrap}>
+                {optionChose === "Post" && <Post/>}
+                {optionChose === "Delete" && <Delete/>}
+            </div>
+        </main>
+    );
 };
 
 export default ModPage;
